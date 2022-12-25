6b0456eb9ab8d7ec85c9bf7eb38c11cb85045dd9
king258436 <king258436@naver.com>
2022-12-25 21:54:02 +0900

    [baekjoon] 아! | 메모리: 1112 KB, 시간: 0 ms

---
+++ baekjoon/아!.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char a[1000] ,b[1000];
+    scanf("%s",a);
+    scanf("%s",b);
+    if(strlen(a)>=strlen(b)){
+        printf("go");
+    }
+    else printf("no");
+    return 0;
+
+}
