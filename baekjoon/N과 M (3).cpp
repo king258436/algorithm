21b8352d061eb30657ec593c9eeaf3a510c35bb9
king258436 <king258436@naver.com>
2023-01-26 22:17:42 +0900

    [baekjoon] N과 M (3) | 메모리: 1116 KB, 시간: 492 ms

---
+++ baekjoon/N과 M (3).cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int n, m;
+int result[10];
+int visited[10];
+
+void dfs(int depth) {
+    if (depth == m) {
+        for (int i = 0; i < m; i++) {
+            printf("%d ", result[i]);
+        }
+        printf("\n");
+    } else {
+        for (int i = 1; i <= n; i++) {
+            if (visited[i] == 0) {
+                result[depth] = i;
+                dfs(depth + 1);
+            }
+        }
+    }
+}
+
+int main() {
+    int i, j;
+    scanf("%d %d", &n, &m);
+    dfs(0);
+    return 0;
+}
