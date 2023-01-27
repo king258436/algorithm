5f65de5098fa369f903a5a366008e12106af12a6
king258436 <king258436@naver.com>
2023-01-27 09:09:47 +0900

    [baekjoon] N과 M (6) | 메모리: 1156 KB, 시간: 0 ms

---
+++ baekjoon/N과 M (6).cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int n, m;
+int result[10];
+int visited[10001];
+int arr[10];
+
+int compare(const void *a, const void *b) { return *(int *)a - *(int *)b; }
+
+void dfs(int depth,int min) {
+    if (depth == m) {
+        for (int i = 0; i < m; i++) {
+            printf("%d ", result[i]);
+        }
+        printf("\n");
+    } 
+    else {
+        for (int i = 0; i < n; i++) {
+            if (visited[arr[i]] == 0&&arr[i]>min) {
+                result[depth]=arr[i];
+                visited[arr[i]]=1;
+                dfs(depth + 1,result[depth]);
+                visited[arr[i]]=0;
+            }
+        }
+    }
+}
+
+int main() {
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    qsort(arr, n, sizeof(int), compare);
+
+    dfs(0,0);
+    return 0;
+}
