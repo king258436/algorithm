27ebdf8a2803ce3641dac93c6d687bfa4b08f93c
king258436 <king258436@naver.com>
2023-02-19 20:56:08 +0900

    [baekjoon] 하노이 탑 이동 순서 | 메모리: 2208 KB, 시간: 172 ms

---
+++ baekjoon/하노이 탑 이동 순서.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+void hanoi(int n, int from, int by, int to) {
+    if (n == 1)
+        cout << from << ' ' << to << '\n';
+    else {
+        hanoi(n - 1, from, to, by);
+        cout << from << ' ' << to << '\n';
+        hanoi(n - 1, by, from, to);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << (int)pow(2, n) - 1<<'\n';
+    hanoi(n, 1, 2, 3);
+}
