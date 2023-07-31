c88086d010272fa50ef9cd8b2225ceb273a60559
king258436 <king258436@naver.com>
2023-07-31 13:11:20 +0900

    [baekjoon] 별 찍기 - 6 | 메모리: 2020 KB, 시간: 0 ms

---
+++ baekjoon/별 찍기 - 6 별 찍기 - 6.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int n;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << ' ';
+        }
+        for (int j = i; j < 2 * n - 1 - i; j++) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
