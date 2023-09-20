6ca9c602ca1b54255dbe17cac365f21864403c92
king258436 <king258436@naver.com>
2023-09-20 22:20:15 +0900

    [baekjoon] 피보나치 수 2 | 메모리: 2020 KB, 시간: 0 ms

---
+++ baekjoon/피보나치 수 2 피보나치 수 2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+long long arr[100];
+
+int main() {
+    int n;
+    cin >> n;
+    arr[0] = 0;
+    arr[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+    cout << arr[n];
+}
