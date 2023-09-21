68028af6ad4395538ffe503f8cd486b4d732253c
king258436 <king258436@naver.com>
2023-09-21 20:27:33 +0900

    [baekjoon] 2×n 타일링 | 메모리: 2024 KB, 시간: 0 ms

---
+++ baekjoon/11726 2×n 타일링.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+#define INF 10007
+using namespace std;
+int dp[1001];
+
+int main() {
+    int n;
+    cin >> n;
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % INF;
+    }
+    cout << dp[n];
+}
